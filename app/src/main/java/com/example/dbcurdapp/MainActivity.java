93a0a2895f50b1fad613cad7e68b542f53203c9f
Mohammad Abdullah <bsef19a531@pucit.edu.pk>
2022-08-20 14:21:39 +0500

    Added Read and Add onclick listners

--- app/src/main/java/com/example/dbcurdapp/MainActivity.java
+++ app/src/main/java/com/example/dbcurdapp/MainActivity.java
@@ -3,12 +3,63 @@ package com.example.dbcurdapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button addButton, viewAllButton;
+    TextView nameTxtView, rollTxtView;
+    Switch switchState;
+    ListView listViewStudent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addButton = findViewById(R.id.addBtn);
+        viewAllButton =  findViewById(R.id.viewAllBtn);
+        nameTxtView = findViewById(R.id.studentTxtView);
+        rollTxtView = findViewById(R.id.RollNoTxtView);
+        switchState = findViewById(R.id.switchState);
+        listViewStudent = findViewById(R.id.studentListView);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            StudentModel studentModel;
+
+            @Override
+            public void onClick(View v) {
+                try {
+                    studentModel = new StudentModel(nameTxtView.getText().toString(), Integer.parseInt(rollTxtView.getText().toString()), switchState.isChecked());
+                    //Toast.makeText(MainActivity.this, studentModel.toString(), Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Error while Adding", Toast.LENGTH_SHORT).show();
+                }
+                DBHelper dbHelper  = new DBHelper(MainActivity.this);
+                dbHelper.addStudent(studentModel);
+            }
+        });
+
+        viewAllButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbHelper = new DBHelper(MainActivity.this);
+                List<StudentModel> list = dbHelper.getAllStudents();
+                ArrayAdapter arrayAdapter = new ArrayAdapter<StudentModel>
+                        (MainActivity.this, android.R.layout.simple_list_item_1,list);
+                listViewStudent.setAdapter(arrayAdapter);
+
+            }
+        });
     }
+
+
 }
